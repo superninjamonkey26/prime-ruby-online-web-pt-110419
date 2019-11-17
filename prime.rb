6f563ef537e308d05c56b8ebdac8fd02390d2f3c
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-17 06:04:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@ require 'prime'
 def prime?(n)
   if n <= 1
     return false
-  elsif n <= 3
-    return true
-  else (1..10).to_a { |i| return false if n % i == 0}
+  #elsif n <= 3
+  #  return true
+ # else (1..10).to_a { |i| return false if n % i == 0}
     #{|i| i.to_a(string)}
   end
 end
