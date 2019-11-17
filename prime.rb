d6b079d80183e531eedce5bdec28888331714933
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-17 06:06:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(n)
     return false
   elsif n <= 3
     return true
-  else (2..n-1).none? |divisor| n %
+  else (2..n-1).none? |divisor| n % divisor == 0
     to_a { |i| return false if n % i == 0}
     #{|i| i.to_a(string)}
   end
